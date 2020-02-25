e8211afae06be590a9b984ec8406d61e3ddc7644
Rodrigo Gonçalves <rodrigosg2000@gmail.com>
2020-02-25 03:04:47 -0300

    Aula 12

--- App.js
+++ App.js
@@ -52,6 +52,17 @@ export default class App extends Component {
     this.setState({board, lost, won});
   };
 
+  onSelectField = (row, column) => {
+    const board = cloneBoard(this.state.board);
+    invertFlag(board, row, column);
+    const won = wonGame(board);
+    if (won) {
+      Alert.alert('Parabéns', 'Você venceu!');
+    }
+
+    this.setState({board, won});
+  };
+
   render() {
     return (
       <View style={styles.container}>
@@ -60,7 +71,11 @@ export default class App extends Component {
           Tamanho da grade: {params.getRowsAmount()}x{params.getColumnsAmount()}
         </Text>
         <View style={styles.board}>
-          <MineField board={this.state.board} onOpenField={this.onOpenField} />
+          <MineField
+            board={this.state.board}
+            onOpenField={this.onOpenField}
+            onSelect={this.onSelectField}
+          />
         </View>
       </View>
     );
