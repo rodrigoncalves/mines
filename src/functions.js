e1e92185e1cd2e9551f1ac3434f6805bff3f9acb
Rodrigo Gonçalves <rodrigosg2000@gmail.com>
2020-02-25 02:16:31 -0300

    Aula 10

--- src/functions.js
+++ src/functions.js
@@ -16,7 +16,8 @@ const createBoard = (rows, columns) =>
         }),
     );
 
-const spreadMines = (board, minesAmount) => {
+const spreadMines = (originalBoard, minesAmount) => {
+  const board = cloneBoard(originalBoard);
   const rows = board.length;
   const columns = board[0].length;
   let minesPlanted = 0;
@@ -32,8 +33,83 @@ const spreadMines = (board, minesAmount) => {
   }
 };
 
-export const createMinedBoard = (rows, columns, minesAmount) => {
+const createMinedBoard = (rows, columns, minesAmount) => {
   const board = createBoard(rows, columns);
-  spreadMines(board, minesAmount);
-  return board;
+  return spreadMines(board, minesAmount);
+};
+
+const cloneBoard = board => {
+  board.map(rows =>
+    rows.map(field => {
+      return {...field};
+    }),
+  );
+};
+
+const getNeighbors = (board, row, column) => {
+  const neighbors = [];
+  const rows = [row - 1, row, row + 1];
+  const columns = [column - 1, column, column + 1];
+
+  rows.forEach(r => {
+    columns.forEach(c => {
+      const different = r !== row || c !== column;
+      const validRow = r >= 0 && r < board.length;
+      const validColumn = c >= 0 && c < board[0].length;
+      if (different && validRow && validColumn) {
+        neighbors.push(board[r][c]);
+      }
+    });
+  });
+  return neighbors;
+};
+
+const safeNeighborhood = (board, row, column) => {
+  const safes = (result, neighbor) => result && !neighbor.mined;
+  return getNeighbors(board, row, column).reduce(safes, true);
+};
+
+const openField = (board, row, column) => {
+  const field = board[row][column];
+  if (!field.opened) {
+    field.opened = true;
+    if (field.mined) {
+      field.exploded = true;
+    } else if (safeNeighborhood(board, row, column)) {
+      getNeighbors(board, row, column).forEach(n =>
+        openField(board, n.row, n.column),
+      );
+    } else {
+      const neighbors = getNeighbors(board, row, column);
+      field.nearMines = neighbors.filter(n => n.mined).length;
+    }
+  }
+};
+
+const fields = board => [].concat(...board);
+const hadExplosion = board => fields(board).some(field => field.exploded);
+const pending = field =>
+  (field.mined && !field.flagged) || (!field.mined && !field.opened);
+const wonGame = board => fields(board).every(!pending);
+const showMines = board =>
+  fields(board)
+    .field(field => field.mined)
+    .forEach(field => (field.openField = true));
+
+const invertFlag = (board, row, column) => {
+  const field = board[row][column];
+  field.flagged = !field.flagged;
+};
+
+const flagsUsed = board => fields(board).filter(field => field.flagged).length;
+
+export {
+  createMinedBoard,
+  cloneBoard,
+  openField,
+  hadExplosion,
+  wonGame,
+  showMines,
+  invertFlag,
+  flagsUsed,
 };
